babbdd533f686d3166f86a13ad9754784af602e0
xzh <tianshangke@qq.com>
2018-12-30 20:31:27 +0800

    structure change

---
+++ Crawlnalyzer/utilities/config.py
@@ -0,0 +1,10 @@
+
+mysql_config = {  'host': '132.232.149.138',
+                       'port': 3306,
+                       'database': 'crawler_data',
+                       'user': 'xzh',
+                       'password': '!QAZ1qaz',
+                       'charset': 'utf8mb4'}
+
+weibo_config = {'user':'18813108431',
+                'password' : 'Hlzhdsh5'}
