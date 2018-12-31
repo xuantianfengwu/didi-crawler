bc8cd307563662e0a95f7936d89be72f77fa68d3
xuantianfengwu <tianshangke@qq.com>
2018-12-31 21:08:35 +0800

    delete config.py

--- Crawlnalyzer/utilities/config.py
+++
@@ -1,8 +0,0 @@
-mysql_config = {'host': '212.64.86.50',
-                'port': 3306,
-                'database': 'crawl_data',
-                'user': 'xzh',
-                'password': '!QAZ1qaz',
-                'charset': 'utf8mb4'}
-
-weibo_config = {'user': '18813108431', 'passwd': 'Hlzhdsh5'}
